c21115a1ea53d8d7f8388a4f7b6fa3746b9401f9
Luktowers <lcs.torss@gmail.com>
2022-05-09 17:51:17 -0300

    erro fix

---
+++ src/aula01/aula01/aula01.java
@@ -0,0 +1,29 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class aula01 {
+    public static void main(String[] args) {
+
+        Scanner inputUser = new Scanner(System.in);
+        
+        System.out.println("Qual é o seu nome?");
+        String nome = inputUser.nextLine();
+
+        System.out.println("Há quantos anos trabalha aqui? ");
+        int tempo = inputUser.nextInt();
+
+        System.out.println("Qual é o seu salário atual? ");
+        double salario = inputUser.nextDouble();
+        inputUser.close();
+
+        double novoSalario = salario + (10 * salario / 100);
+
+
+        if (tempo >=10) {
+            System.out.printf("%s, seu novo salario é de R$ %.2f", nome, novoSalario);            
+        } else {
+            System.out.println(nome + ", você ainda não tem direito a receber aumento");
+        }
+    }
+}
